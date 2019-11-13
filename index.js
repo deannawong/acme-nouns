ac5934c0732f97f2fcdc15782dfd252f581591ab
Deanna Wong <dwong221@gmail.com>
2019-11-13 16:59:52 -0500

    DRY routes for getting apis

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { Places, People, Things } = models;
 app.get("/", (req, res, next) => {
   res.sendFile(path.join(__dirname, "./index.html"));
 });
-
+// Posting???
 app.post("/api/people", (req, res, next) => {
   People.create(req.body)
     .then(person => people.findAll())
@@ -25,37 +25,53 @@ app.post("/api/people", (req, res, next) => {
     });
 });
 
-app.get("/api/places", (req, res, next) => {
-  Places.findAll({ include: [People] })
-    .then(places => {
-      res.send(places);
-    })
-    .catch(e => {
-      console.log("Error getting all places", e);
-      next(e);
-    });
-});
+// app.get("/api/places", (req, res, next) => {
+//   Places.findAll({ include: [People] })
+//     .then(places => {
+//       res.send(places);
+//     })
+//     .catch(e => {
+//       console.log("Error getting all places", e);
+//       next(e);
+//     });
+// });
 
-app.get("/api/people", (req, res, next) => {
-  People.findAll({ include: [Things] })
-    .then(people => {
-      res.send(people);
-    })
-    .catch(e => {
-      console.log("Error getting all people", e);
-      next(e);
-    });
-});
+// app.get("/api/people", (req, res, next) => {
+//   People.findAll({ include: [Things] })
+//     .then(people => {
+//       res.send(people);
+//     })
+//     .catch(e => {
+//       console.log("Error getting all people", e);
+//       next(e);
+//     });
+// });
 
-app.get("/api/things", (req, res, next) => {
-  Things.findAll()
-    .then(things => {
-      res.send(things);
-    })
-    .catch(e => {
-      console.log("Error getting all things", e);
-      next(e);
-    });
+// app.get("/api/things", (req, res, next) => {
+//   Things.findAll()
+//     .then(things => {
+//       res.send(things);
+//     })
+//     .catch(e => {
+//       console.log("Error getting all things", e);
+//       next(e);
+//     });
+// });
+
+//USING Object.entries and forEach to grab all models // dry routes
+
+Object.entries(db.models).forEach(([name, model]) => {
+  app.get(`/api/${name}`, (req, res, next) => {
+    model
+      .findAll()
+      .then(item => {
+        res.send(item);
+      })
+      .catch(e => {
+        console.log(chalk.red(`Error getting all ${name}`));
+        next(e);
+      });
+  });
 });
 
 app.get("/api/people/:id", (req, res, next) => {
@@ -77,6 +93,7 @@ db.sync({ force: true })
   .then(() => seed())
   .then(() => {
     app.listen(PORT, () => {
+      console.log("db models", Object.entries(db.models));
       console.log(
         chalk.cyan(`Application started on http://localhost:${PORT}`)
       );
